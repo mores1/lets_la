70f9b96023908eafdd1dd04d0cea80fec75cb397
mores1 <sf1021938@gmail.com>
2022-06-12 14:42:32 +0000

    0612

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root :to => "homes#top"
+  
+  get 'login' => "sessions#new"
+  post 'login' => "sessions#create"
+  delete 'logout' => "sessions#destroy"
 
   resources :users, only: [:new, :create, :index, :edit, :update]
   resources :sessions, only: [:create]
@@ -9,6 +13,7 @@ Rails.application.routes.draw do
     resources :spot_comments, only: [:create, :destroy]
     resource :likes, only: [:create, :destroy]
   end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
