27ea92300cdc683ad9c0c12b684d7a4380771efb
mores1 <sf1021938@gmail.com>
2022-06-13 11:05:04 +0000

    yet0613

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,11 +1,27 @@
 class SpotsController < ApplicationController
-  
+
   before_action :signin_user?
 
   def new
+    @user = @current_user
+    @spot = Spot.new
+  end
+
+  def create
+    @spot = Spot.new(spot_params)
+    @user = @current_user
+    @spot.user_id = @user.id
+    if @spot.lat ==0 || @spot.lng == 0 #この場合場所指定忘れの可能性大
+      redirect_to request.referer
+    elsif @spot.save
+      redirect_to spots_path
+    else
+      redirect_to request.referer
+    end
   end
 
   def index
+    @spots = Spot.all
   end
 
   def show
@@ -13,4 +29,11 @@ class SpotsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def spot_params
+    params.required(:spot).permit(:user_id, :title, :body, :lat, :lng, :image_id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
-  has_secure_password #dbに依存せずpass+confirmpass使用可能に(Gem'bcrypt'で使用できるようになっている)
-
+    
+    has_many :spots, dependent: :destroy
+    has_secure_password #dbに依存せずpass+confirmpass使用可能に(Gem'bcrypt'で使用できるようになっている)
 end
